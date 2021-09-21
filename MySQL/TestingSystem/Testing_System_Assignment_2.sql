93c8eadb82756b640bdf75267204afa8c2bf95c6
thanhvinh8192 <thanhvinh8192@gmail.com>
2021-09-22 01:45:23 +0900

    format assignment 2

--- MySQL/TestingSystem/Testing_System_Assignment_2.sql
+++ MySQL/TestingSystem/Testing_System_Assignment_2.sql
@@ -1,59 +1,59 @@
 -- Insert data to Department Table --
 INSERT INTO `department`(department_name)
 	VALUES 				('Nhân sự'),
-                        ('Kỹ thuật'),
+						('Kỹ thuật'),
 						('Kế toán'),
 						('Marketing'),
-                        ('Kinh doanh'),
-                        ('Đào tạo')
+						('Kinh doanh'),
+						('Đào tạo')
 ;
 
 -- Insert data to Position Table --
 INSERT INTO `position`	(position_name)
 	VALUES				('Dev'),
 						('Test'),
-                        ('Scrum Master'),
-                        ('PM')
+						('Scrum Master'),
+						('PM')
 ;
         
 -- Insert data to Account Table --
 INSERT INTO `account`	(email, 					username, 		fullname, 				department_id, 	position_id, 	create_date)	
 	VALUES				('longtrinh@vti.com.vn', 	'longtrinh', 	'Trịng Hoài Long', 		1, 				3, 				'2019-12-01'),
 						('baonguyen@vti.com.vn', 	'baonguyen', 	'Nguyễn Quốc Bảo',		2, 				1, 				CURDATE()),
-                        ('chauduong@vti.com.vn', 	'chauduong', 	'Dương Minh Châu',		3, 				2, 				'2020-12-01'),
-                        ('vudinh@vti.com.vn', 		'vudinh', 		'Đinh Thế Vũ',			4, 				1, 				'2019-12-01'),
-                        ('thanhduong@vti.com.vn', 	'thanduong', 	'Dương Quốc Thành',		4, 				4, 				'2021-12-01'),
-                        ('vti_5@vti.com.vn', 		'account5', 	'Ho Van Tinh', 			2, 				2, 				'2021-07-01'),
+						('chauduong@vti.com.vn', 	'chauduong', 	'Dương Minh Châu',		3, 				2, 				'2020-12-01'),
+						('vudinh@vti.com.vn', 		'vudinh', 		'Đinh Thế Vũ',			4, 				1, 				'2019-12-01'),
+						('thanhduong@vti.com.vn', 	'thanduong', 	'Dương Quốc Thành',		4, 				4, 				'2021-12-01'),
+						('vti_5@vti.com.vn', 		'account5', 	'Ho Van Tinh', 			2, 				2, 				'2021-07-01'),
 						('vti_6@vti.com.vn', 		'account_vti6', 'Cao Thai Son', 		3, 				1, 				CURDATE()	),
 						('vti_7@vti.com.vn', 		'account_vti7', 'Tam Thất Tùng', 		1, 				3, 				'2020-10-01'),
 						('vti_8@vti.com.vn', 		'account_vti8', 'Nguyen Quynh Thu', 	5, 				4, 				'2019-04-01'),
-                        ('trungnguyen@vti.com.vn', 	'trungnguyen', 	'Nguyễn Thành Trung',	5 , 			1, 				CURDATE()	)
+						('trungnguyen@vti.com.vn', 	'trungnguyen', 	'Nguyễn Thành Trung',	5 , 			1, 				CURDATE()	)
 ;
 
 -- Insert data to Group Table --
 INSERT INTO `group`	(group_name, 	creator_id, create_date)
 	VALUES 			('Group VTI 1', 1, 			CURDATE()	),
 					('Group VTI 2', 2, 			'2020-04-03'),
-                    ('Group VTI 3', 3, 			CURDATE()	),
-                    ('Group VTI 4', 4, 			'2019-05-03'),
-                    ('Group VTI 5', 5, 			CURDATE()	),
-                    ('Group VTI 6', 6, 			'2019-05-03')
+					('Group VTI 3', 3, 			CURDATE()	),
+					('Group VTI 4', 4, 			'2019-05-03'),
+					('Group VTI 5', 5, 			CURDATE()	),
+					('Group VTI 6', 6, 			'2019-05-03')
 ;
 
 -- Insert data to Group_Account Table --                    
 INSERT INTO `group_account`(group_id, 	account_id, join_date)
 	VALUES 					(1, 		1, 			NOW()		),
 							(2, 		3, 			'2020-09-21'),
-                            (6, 		5, 			'2021-09-21'),
-                            (3, 		7, 			'2021-09-22'),
-                            (3, 		9, 			'2021-09-22'),
-                            (5, 		2, 			NOW()		),
-                            (4, 		4, 			NOW()		),
-                            (1, 		6, 			'2021-09-20'),
-                            (5, 		8, 			'2021-09-21'),
-                            (1, 		10, 		'2021-09-22'),
-                            (6, 		4, 			NOW()		),
-                            (4, 		2, 			'2021-09-21')
+							(6, 		5, 			'2021-09-21'),
+							(3, 		7, 			'2021-09-22'),
+							(3, 		9, 			'2021-09-22'),
+							(5, 		2, 			NOW()		),
+							(4, 		4, 			NOW()		),
+							(1, 		6, 			'2021-09-20'),
+							(5, 		8, 			'2021-09-21'),
+							(1, 		10, 		'2021-09-22'),
+							(6, 		4, 			NOW()		),
+							(4, 		2, 			'2021-09-21')
 ;
 
 -- Insert data to Type_Question Table --
@@ -97,14 +97,14 @@ INSERT INTO `answer`(content, 							question_id, is_correct)
 					('Câu trả lời 2 - question SQL 2', 		2, 			0),
 					('Câu trả lời 1 - question Postman', 	5, 			0),
 					('Câu trả lời 4 - question SQL 2', 		2, 			1),
-					('Câu trả lời 1 - question JAVA 1', 	3, 			0),
-					('Câu trả lời 2 - question JAVA 1', 	3, 			1),
-					('Câu trả lời 1 - question JAVA 2', 	4, 			0),
-					('Câu trả lời 2 - question JAVA 2', 	4, 			0),
-					('Câu trả lời 3 - question JAVA 2', 	4, 			0),
-					('Câu trả lời 4 - question JAVA 2', 	4, 			1),
+					('Câu trả lời 1 - question JAVA 1', 		3, 			0),
+					('Câu trả lời 2 - question JAVA 1', 		3, 			1),
+					('Câu trả lời 1 - question JAVA 2', 		4, 			0),
+					('Câu trả lời 2 - question JAVA 2', 		4, 			0),
+					('Câu trả lời 3 - question JAVA 2', 		4, 			0),
+					('Câu trả lời 4 - question JAVA 2', 		4, 			1),
 					('Câu trả lời 1 - question Ruby', 		8, 			1),
-					('Câu trả lời 2 - question HTML 2', 	5, 			0)
+					('Câu trả lời 2 - question HTML 2', 		5, 			0)
 ;
 
 -- Insert data to Exam Table --
